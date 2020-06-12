749a39a737610570961f511f7ba321cc118e05ce
Bucky Schwarz <d.w.schwarz@gmail.com>
2020-06-12 11:02:55 -0700

    test: mock fetch in jestSetup

--- ui/jestSetup.ts
+++ ui/jestSetup.ts
@@ -1,12 +1,15 @@
 import {cleanup} from '@testing-library/react'
 import 'intersection-observer'
 import MutationObserver from 'mutation-observer'
+import fetchMock from 'jest-fetch-mock'
+
 // Adds MutationObserver as a polyfill for testing
 window.MutationObserver = MutationObserver
 
+fetchMock.enableMocks()
 jest.mock('honeybadger-js', () => () => null)
 
-process.env.API_PREFIX = '/'
+process.env.API_PREFIX = 'http://example.com/'
 // cleans up state between @testing-library/react tests
 afterEach(() => {
   cleanup()
